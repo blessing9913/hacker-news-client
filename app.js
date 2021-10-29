7c420c5988c2754b0a76600d2cfd6ecd307f93ab
blessing9913 <blessing9913@gmail.com>
2021-10-29 11:19:09 +0900

    Add page nation

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_RUL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+  currentPage: 1,
+}
 
 function getData(url) {
   ajax.open('GET', url, false); // async = false 동기처리
@@ -16,17 +19,24 @@ function newsFeed() {
 
   newsList.push('<ul>');
 
-  for(let i = 0; i < newsFeed.length; i++) {
+  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
       <li>
-        <a href="#${newsFeed[i].id}">
+        <a href="#/show/${newsFeed[i].id}">
           ${newsFeed[i].title} [${newsFeed[i].comments_count}]
         </a>
       </li>
     `);
   }
 
+  console.log(newsFeed.length + '|' + (store.currentPage * 10))
   newsList.push('</ul>');
+  newsList.push(`
+    <div>
+      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+      <a href="#/page/${newsFeed.length > store.currentPage * 10 ? store.currentPage + 1 : store.currentPage}">다음 페이지</a>
+    </div>
+  `);
 
   container.innerHTML = newsList.join('');
 }
@@ -36,7 +46,7 @@ const ul = document.createElement('ul');
 
 // 글 상세
 function newsDetail() {
-  const id = location.hash.substr(1);
+  const id = location.hash.substr(7);
 
   const newsContent = getData(CONTENT_RUL.replace('@id', id));
 
@@ -44,7 +54,7 @@ function newsDetail() {
     <h1>${newsContent.title}</h1>
 
     <div>
-      <a href="#">목록으로</div>
+      <a href="#/page/${store.currentPage}">목록으로</div>
     </div>
   `;
 }
@@ -54,6 +64,9 @@ function router() {
   const routePath = location.hash;
 
   if(routePath === '') { // location.hash 에 #만 있을 경우 빈 값을 반환 함
+    newsFeed();
+  } else if(routePath.indexOf('/page/') >= 0) {
+    store.currentPage = Number(routePath.substr(7));
     newsFeed();
   } else {
     newsDetail();
